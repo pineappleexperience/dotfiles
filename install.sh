08953a1c7bd49f33deebfabb6595661f95dee7f2
Norman Lorenz <nlo@xima.de>
2025-05-03 22:02:13 +0200

    Added install script for easier installation of the config files using GNU stow

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# List of folders you want to stow
+FOLDERS=("aerospace" "ideavim" "nvim" "tmux" "zsh")
+
+# Target directory
+TARGET_DIR="$HOME"
+
+echo "Installing dotfiles..."
+
+# Run stow for each folder
+for folder in "${FOLDERS[@]}"; do
+  echo "🔁 (Re-)stowing $folder -> $TARGET_DIR"
+  stow -R -t "$TARGET_DIR" "$folder"
+done
+
+echo "✅ Dotfiles installation complete."
+
