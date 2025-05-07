8d06e3d7a9ed50031bca8148daae137b83a68807
Norman Lorenz <nlo@xima.de>
2025-05-07 21:08:13 +0200

    Removed 'work' tmux startup session

--- zsh/.tmux-startup.sh
+++ zsh/.tmux-startup.sh
@@ -6,12 +6,6 @@ if [ $? != 0 ]; then
   tmux new-session -ds projects -c "$HOME/projects"
 fi
 
-# Start a session "work" in ~/projects/work if it doesn't exist
-tmux has-session -t work 2>/dev/null
-if [ $? != 0 ]; then
-  tmux new-session -ds work -c "$HOME/projects/work"
-fi
-
 # Start a session "notes" in ~/Documents/notes if it doesn't exist
 tmux has-session -t notes 2>/dev/null
 if [ $? != 0 ]; then
