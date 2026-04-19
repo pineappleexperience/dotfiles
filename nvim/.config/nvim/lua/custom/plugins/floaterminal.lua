8ff665f09cb4b0b85a68186c7d52a3d6677e47fb
Norman Lorenz <knockuar@gmail.com>
2026-04-19 17:21:23 +0200

    terminal opens at the bottom

--- nvim/.config/nvim/lua/custom/plugins/floaterminal.lua
+++ nvim/.config/nvim/lua/custom/plugins/floaterminal.lua
@@ -51,4 +51,19 @@ end
 vim.api.nvim_create_user_command("Floaterminal", toggle_terminal, {})
 vim.keymap.set({ "n", "t" }, "<leader>tt", toggle_terminal, { desc = "[T]erminal" })
 
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	end,
+})
+
+vim.keymap.set("n", "<leader>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 15)
+end, { desc = "[T]erminal" })
+
 return {}
