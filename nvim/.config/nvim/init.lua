18ee23d6dd34e63b5ea393415d00cd7adb7b0519
Norman Lorenz <knockuar@gmail.com>
2026-04-21 21:46:08 +0200

    moved kickstart plugins out of kickstart directory

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -815,12 +815,12 @@ require("lazy").setup({
 	--  Here are some example plugins that I've included in the Kickstart repository.
 	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
 	--
-	require("kickstart.plugins.debug"),
-	-- require 'kickstart.plugins.indent_line',
-	-- require 'kickstart.plugins.lint',
-	-- require 'kickstart.plugins.autopairs',
-	require("kickstart.plugins.neo-tree"),
-	require("kickstart.plugins.gitsigns"), -- adds gitsigns recommend keymaps
+	require("plugins.debug"),
+	-- require 'plugins.indent_line',
+	-- require 'plugins.lint',
+	-- require 'plugins.autopairs',
+	require("plugins.neo-tree"),
+	require("plugins.gitsigns"), -- adds gitsigns recommend keymaps
 
 	{ import = "custom.plugins" },
 }, {
