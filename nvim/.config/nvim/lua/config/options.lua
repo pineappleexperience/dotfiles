8b27740092009a2b997333c64b2242cb2530690e
Norman Lorenz <knockuar@gmail.com>
2026-04-21 20:37:06 +0200

    refactored vim config a bit to use separate files for options and keymaps

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,52 @@
+vim.g.have_nerd_font = true
+
+-- See `:help vim.o`
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.mouse = "a" -- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.showmode = false -- Don't show the mode, since it's already in the status line
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
+
+vim.o.breakindent = true
+vim.o.undofile = true -- Save undo history
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = "yes"
+
+vim.o.updatetime = 250 -- Decrease update time
+vim.o.timeoutlen = 300 -- Decrease mapped sequence wait time
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.o.inccommand = "split" -- Preview substitutions live, as you type!
+
+vim.o.cursorline = true
+vim.o.scrolloff = 15
+vim.o.confirm = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
