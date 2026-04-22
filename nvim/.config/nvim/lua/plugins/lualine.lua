6db54e59930e41700c186e1a830ad2e66f8937e8
Norman Lorenz <knockuar@gmail.com>
2026-04-22 21:46:21 +0200

    Lualine config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -38,7 +38,12 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
@@ -46,7 +51,12 @@ return {
 			inactive_sections = {
 				lualine_a = {},
 				lualine_b = {},
-				lualine_c = { "filename" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
 				lualine_x = { "location" },
 				lualine_y = {},
 				lualine_z = {},
